e17616744cafdafbd7cd3f32743813387d73892d
pburyanov <p_buryanov-itssv@kw.com>
2024-01-19 18:40:05 +0200

    : new file structure

--- init.lua
+++ init.lua
@@ -1,6 +1,21 @@
-require("dgcat.options")
-require("dgcat.keymaps")
-require("dgcat.lazy")
+-- import settings
+require("options")
+require("keymaps")
+
+-- bootstrap lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
 
 -- vim.cmd.colorscheme("kanagawa-wave")
 vim.cmd.colorscheme("horizon")
